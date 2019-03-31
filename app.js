6d807fde65724ee0f861b3fbf80c3c0a5339f7d6
Dave Guarino <dave@codeforamerica.org>
2019-03-31 09:41:24 -0700

    add template and route for random_point

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
 
+app.post('/random_point', (req, res) => res.render('random_point'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

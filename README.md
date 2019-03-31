1536b15ccc8e2f6d7fe8c66117f3a7c6ebc1f936
Dave Guarino <dave@codeforamerica.org>
2019-03-31 11:38:56 -0700

    basic testing set up

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+To run tests, run `npm test`
+
--- test/test.js
+++ test/test.js
@@ -2,10 +2,13 @@ var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
 
+var tools = require('../js/tools')
+
 describe('getRandomPointInMultiPolygon()', function() {
     it('should return a single point', function() {
-        //point = getRandomPointInMultiPolygon(geojson);
-        point = [1,1];
+        geojson = 'fakefornow';
+        point = tools.getRandomPointInMultiPolygon(geojson);
+        //point = [1,1];
         expect(point).to.be.a('array');
         expect(point.length).to.equal(2);
       }

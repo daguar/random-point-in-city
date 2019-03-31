14f7e4bc5e912ea57a0418e1b1913e4c9f6291aa
Dave Guarino <dave@codeforamerica.org>
2019-03-31 12:26:53 -0700

    basic logic of getting random point in multipolygon working

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,27 @@
+var turf = require('@turf/turf');
+
 module.exports = {
-  getRandomPointInMultiPolygon: function(multipolygon) {
-    return [0,0];
+  getRandomPointInMultiPolygon: getRandomPointInMultiPolygon
+}
+
+function getRandomPointInMultiPolygon(multipolygon) {
+  boundingBox = turf.bbox(multipolygon);
+  x_min = boundingBox[0];
+  y_min = boundingBox[1];
+  x_max = boundingBox[2];
+  y_max = boundingBox[3];
+
+  lat = y_min + (Math.random() * (y_max - y_min));
+  lng = x_min + (Math.random() * (x_max - x_min));
+
+  point = turf.point([lng, lat]);
+  inside = turf.inside(point, multipolygon.geometries[0]);
+
+  if(inside) {
+    return point.geometry.coordinates;
+  }
+  else {
+    return getRandomPointInMultiPolygon(multipolygon);
   }
 }
+
--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
 var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
+var fs = require('fs');
 
 var tools = require('../js/tools')
 
 describe('getRandomPointInMultiPolygon()', function() {
-    it('should return a single point', function() {
-        geojson = 'fakefornow';
-        point = tools.getRandomPointInMultiPolygon(geojson);
-        //point = [1,1];
-        expect(point).to.be.a('array');
-        expect(point.length).to.equal(2);
-      }
-    );
-  }
-);
+  it('should return a single point', function() {
+    rawData = fs.readFileSync('./test/fixtures/oakland.geojson');
+    geojson = JSON.parse(rawData);
+    point = tools.getRandomPointInMultiPolygon(geojson);
+    expect(point).to.be.a('array');
+    expect(point.length).to.equal(2);
+  });
+});
 
